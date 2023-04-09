9042a2f0a94c78127db6fbb89b8b67eab7302a09
dalmamad <dal.mohamad97@gmail.com>
2023-04-09 06:30:28 +0200

    adding test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,64 +1,98 @@
-// import { io } from 'socket.io-client';
-// import { Server } from 'socket.io';
-// import { setListeners, On, Listener } from '../src/index';
-//
-// const socketClient = io('ws://localhost:3000');
-//
-// beforeAll(() => {
-//   @Listener('/msg')
-//   class Class {
-//     @On('/new')
-//     async method(socket: any, data: any, callback: any) {
-//       console.log(socket);
-//       console.log(data);
-//       // callback({ status: 'ok' });
-//     }
-//   }
-//
-//   const ioServer = new Server(3000);
-//   ioServer.on('connection', (socket) => {
-//     socket.on('hii', (data) => {
-//       console.log(data);
-//     });
-//   });
-// });
-//
-// describe('User controllers', () => {
-//   test('Get data', () => {
-//     socketClient.emit('hii', { hi: 'world' });
-//     expect(1).toEqual(1);
-//     // const res = await request(app).get('/api/data/get');
-//     // expect(res.body.status).toEqual(200);
-//   });
-// });
-//
-//
-//
-// import { Server } from 'socket.io';
-// import { setListeners, On, Listener } from '../src/index';
-//
-// @Listener('/msg')
-// class Class {
-//   @On('/new')
-//   method(socket: any, data: any, callback: any) {
-//     throw 'xxxxxxxxx';
-//     console.log('44444');
-//     // console.log(socket);
-//     console.log(data);
-//     callback({ status: 'ok' });
-//   }
-// }
-//
-// const io = new Server(3000);
-// function errorHandler(err: any, res: any) {
-//   console.log('ssssssss');
-//   console.log(err);
-//   res({ err });
-// }
-// io.on('connection', (socket) => {
-//   socket.on('hii', (data) => {
-//     console.log(data);
-//   });
-//
-//   setListeners(socket, { listeners: [Class] });
-// });
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { io } from 'socket.io-client';
+import { setListeners, On, Listener, getListener, Once } from '../src/index';
+
+describe('my project', () => {
+  let serverIo: any;
+  let serverSocket: any;
+  let clientSocket: any;
+
+  function errorHandler(err: any, res: any) {
+    expect(err).toBe(true);
+    res({ err });
+  }
+
+  beforeEach((done) => {
+    const httpServer = createServer();
+    serverIo = new Server(httpServer);
+    httpServer.listen(3000, () => {
+      clientSocket = io('http://localhost:3000');
+      serverIo.on('connection', (socket: any) => {
+        serverSocket = socket;
+      });
+      clientSocket.on('connect', done);
+    });
+  });
+
+  afterEach(() => {
+    serverIo.close();
+    clientSocket.close();
+  });
+
+  test('testing on method', (done) => {
+    @Listener('/msg')
+    class MyClass {
+      @On('/new')
+      async fn(socket: any, data: any, callback: any) {
+        expect(data).toBe(true);
+        callback({ status: true });
+      }
+    }
+    setListeners(serverSocket, { listeners: [MyClass] });
+
+    clientSocket.emit('/msg/new', true, (data: any) => {
+      expect(data.status).toBe(true);
+      done();
+    });
+  });
+
+  test('decorators without arg and with prefix', (done) => {
+    class MyClass {
+      @On()
+      async fn(socket: any, data: any, callback: any) {
+        expect(data).toBe(true);
+        callback({ status: true });
+      }
+    }
+    setListeners(serverSocket, { listeners: [MyClass] }, { prefix: '/socket' });
+
+    clientSocket.emit('/socket', true, (data: any) => {
+      expect(data.status).toBe(true);
+      done();
+    });
+  });
+
+  test('testing errorHandler', (done) => {
+    @Listener('/msg')
+    class MyClass {
+      @On('/new')
+      async fn(socket: any, data: any, callback: any) {
+        throw true;
+      }
+    }
+    setListeners(
+      serverSocket,
+      { listeners: [MyClass], errorHandler },
+      { prefix: '' }
+    );
+
+    clientSocket.emit('/msg/new', true, (data: any) => {
+      expect(data.err).toBe(true);
+      done();
+    });
+  });
+  test('testing get listeners', (done) => {
+    @Listener('/msg')
+    class MyClass {
+      @On('/new')
+      async fn(socket: any, data: any, callback: any) {
+        throw true;
+      }
+    }
+    setListeners(serverSocket, { listeners: [MyClass] });
+    let ls = getListener(MyClass, 'fn');
+    expect(ls).toBeTruthy();
+    done();
+  });
+});
